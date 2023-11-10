b9025f69e0a1f904ac5d4e44609c5d93e733159e
ManaHASEGAWA <hasegawa-mana607@g.ecc.u-tokyo.ac.jp>
2023-11-10 10:37:34 +0900

    my-app作り直し

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
 
 //各ページのtsxファイルをインポート
-import  Home from "./Home";
 import {Login} from "./Login";
-import  Management from "./Management";
-import  Post from "./Post";
-import  View from "./View";
-import  NotFound from "./Notfound";
 
 
 function App() {
   return (
     <div className="App">
-        <header className="App-header">
-        </header>
-        <Login/>
+      <header className="App-header">
+      </header>
+      <Login/>
     </div>
   );
 }
